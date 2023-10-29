bba7bc953183350387163bacc9ab3fb68d242dd6
Marcin Kukliński <senghe@gmail.com>
2023-10-29 22:42:58 +0100

    Fix PHPSpec issue

--- spec/TheGame/Application/Component/ResourceMiners/Command/ExtractResourcesCommandSpec.php
+++ spec/TheGame/Application/Component/ResourceMiners/Command/ExtractResourcesCommandSpec.php
@@ -12,6 +12,7 @@ final class ExtractResourcesCommandSpec extends ObjectBehavior
     public function let(): void
     {
         $planetId = "d8391d08-7c2e-40d8-b60c-a7c982e5e19e";
+        $resourceId = "b9d07968-c016-4485-9796-7c6f3a3ed2ed";
 
         $this->beConstructedWith($planetId, $resourceId, $amount);
     }
