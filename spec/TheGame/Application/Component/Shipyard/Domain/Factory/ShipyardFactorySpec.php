173c26ab1d977fe6e043bbe5bddc563b704bee91
Marcin Kukliński <marcin.kuklinski@bitbag.pl>
2023-11-11 17:19:46 +0100

    Write next pack of specs for Shipyard component

--- spec/TheGame/Application/Component/Shipyard/Domain/Factory/ShipyardFactorySpec.php
+++ spec/TheGame/Application/Component/Shipyard/Domain/Factory/ShipyardFactorySpec.php
@@ -5,10 +5,32 @@ declare(strict_types=1);
 namespace spec\TheGame\Application\Component\Shipyard\Domain\Factory;
 
 use PhpSpec\ObjectBehavior;
+use TheGame\Application\Component\BuildingConstruction\Domain\BuildingId;
+use TheGame\Application\Component\Shipyard\Domain\ShipyardId;
+use TheGame\Application\SharedKernel\Domain\PlanetId;
+use TheGame\Application\SharedKernel\UuidGeneratorInterface;
 
 final class ShipyardFactorySpec extends ObjectBehavior
 {
-    public function it_creates_shipyard(): void
-    {
+    public function let(
+        UuidGeneratorInterface $uuidGenerator,
+    ): void {
+        $this->beConstructedWith($uuidGenerator);
+    }
+
+    public function it_creates_shipyard(
+        UuidGeneratorInterface $uuidGenerator,
+    ): void {
+        $shipyardId = new ShipyardId("B8670928-FFCA-4D03-AE0F-2BFB605578EB");
+
+        $uuidGenerator->generateNewShipyardId()->willReturn($shipyardId);
+
+        $planetId = new PlanetId("55833879-5897-44CA-9BF4-6835479FBFC0");
+        $buildingId = new BuildingId("79AB0E22-74EB-4C16-A9FB-0CD56A7A42ED");
+
+        $createdShipyard = $this->create($planetId, $buildingId);
+        $createdShipyard->getId()->shouldReturn($shipyardId);
+        $createdShipyard->getPlanetId()->shouldReturn($planetId);
+        $createdShipyard->getBuildingId()->shouldReturn($buildingId);
     }
 }
--- spec/TheGame/Application/Component/Shipyard/EventListener/UpgradeShipyardEventListenerSpec.php
+++ spec/TheGame/Application/Component/Shipyard/EventListener/UpgradeShipyardEventListenerSpec.php
@@ -5,10 +5,90 @@ declare(strict_types=1);
 namespace spec\TheGame\Application\Component\Shipyard\EventListener;
 
 use PhpSpec\ObjectBehavior;
+use TheGame\Application\Component\Balance\Bridge\ShipyardContextInterface;
+use TheGame\Application\Component\BuildingConstruction\Domain\BuildingId;
+use TheGame\Application\Component\BuildingConstruction\Domain\Event\ShipyardConstructionHasBeenFinishedEvent;
+use TheGame\Application\Component\Shipyard\Domain\Entity\Shipyard;
+use TheGame\Application\Component\Shipyard\Domain\Factory\ShipyardFactoryInterface;
+use TheGame\Application\Component\Shipyard\Exception\ShipyardHasNotBeenFoundException;
+use TheGame\Application\Component\Shipyard\ShipyardRepositoryInterface;
+use TheGame\Application\SharedKernel\Domain\PlanetId;
 
 final class UpgradeShipyardEventListenerSpec extends ObjectBehavior
 {
-    public function it_upgrades_shipyard_when_shipyard_building_has_been_upgraded(): void
-    {
+    public function let(
+        ShipyardRepositoryInterface $shipyardRepository,
+        ShipyardFactoryInterface $shipyardFactory,
+        ShipyardContextInterface $shipyardBalanceContext,
+    ): void {
+        $this->beConstructedWith(
+            $shipyardRepository, $shipyardFactory, $shipyardBalanceContext,
+        );
+    }
+
+    public function it_upgrades_shipyard_when_shipyard_building_has_been_upgraded(
+        ShipyardRepositoryInterface $shipyardRepository,
+        ShipyardContextInterface $shipyardBalanceContext,
+        Shipyard $shipyard,
+    ): void {
+        $planetId = "26A52569-15BB-449C-92C0-6B1EB0C1CDD5";
+        $buildingId = "576FAA85-E3BD-4095-8B76-C7C1769488A5";
+        $upgradedLevel = 5;
+
+        $shipyardRepository->findAggregateForBuilding(
+            new PlanetId($planetId), new BuildingId($buildingId),
+        )->willReturn($shipyard);
+
+        $shipyardBalanceContext->getShipyardProductionLimit(5)
+            ->willReturn(20);
+
+        $shipyard->upgrade(20)->shouldBeCalledOnce();
+
+        $event = new ShipyardConstructionHasBeenFinishedEvent(
+            $planetId, $buildingId, $upgradedLevel
+        );
+        $this->__invoke($event);
+    }
+
+    public function it_creates_shipyard_when_shipyard_has_level_1(
+        ShipyardFactoryInterface $shipyardFactory,
+        ShipyardContextInterface $shipyardBalanceContext,
+        Shipyard $shipyard,
+    ): void {
+        $planetId = "26A52569-15BB-449C-92C0-6B1EB0C1CDD5";
+        $buildingId = "576FAA85-E3BD-4095-8B76-C7C1769488A5";
+        $upgradedLevel = 1;
+
+        $shipyardFactory->create(
+            new PlanetId($planetId), new BuildingId($buildingId),
+        )->willReturn($shipyard);
+
+        $shipyardBalanceContext->getShipyardProductionLimit(1)
+            ->willReturn(3);
+
+        $shipyard->upgrade(3)->shouldBeCalledOnce();
+
+        $event = new ShipyardConstructionHasBeenFinishedEvent(
+            $planetId, $buildingId, $upgradedLevel
+        );
+        $this->__invoke($event);
+    }
+
+    public function it_throws_exception_when_shipyard_has_level_greater_than_1_but_not_exist(
+        ShipyardRepositoryInterface $shipyardRepository,
+    ): void {
+        $planetId = "26A52569-15BB-449C-92C0-6B1EB0C1CDD5";
+        $buildingId = "576FAA85-E3BD-4095-8B76-C7C1769488A5";
+        $upgradedLevel = 5;
+
+        $shipyardRepository->findAggregateForBuilding(
+            new PlanetId($planetId), new BuildingId($buildingId),
+        )->willReturn(null);
+
+        $event = new ShipyardConstructionHasBeenFinishedEvent(
+            $planetId, $buildingId, $upgradedLevel
+        );
+        $this->shouldThrow(ShipyardHasNotBeenFoundException::class)
+            ->during('__invoke', [$event]);
     }
 }
